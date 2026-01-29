b413529b8b8c6d699801b5fb5952cb8f73f612a7
Aravind <aravind@gridlex.com>
2026-01-29 22:01:31 +0530

    Add notes-service and update package structures; integrate express in user-service and gateway

---
+++ apps/gateway/src/app.ts
@@ -0,0 +1,10 @@
+import express, { type Express } from 'express';
+import type { Request, Response } from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World, FROM the gateway');
+});
+
+export default app;
--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+import app from './app';
+
+app.listen(3003, () => {
+  console.log('Server is running on port 3003');
+});
